0a03af84c99be97b7252a945d0e95b95e50135ce
Eric Wyne <ecwyne@gmail.com>
2014-12-03 12:03:59 -0800

    working on scoringRules

--- client/templates/rules/scoring.js
+++ client/templates/rules/scoring.js
@@ -1,11 +1,16 @@
+var diffDef = new Tracker.Dependency()
+
 Template.scoringRules.events({
 	'change input': function(e){
-
+		diffDef.changed();
 	}
 });
 
 Template.scoringRules.helpers({
-
+	pointDiff: function(team){
+		diffDef.depend()
+		return Math.abs(parseInt($('#' + team + '1').val()) - $('#' + team + '2').val());
+	}
 });
 
 Template.scoringRules.rendered = function(){
@@ -13,4 +18,5 @@ Template.scoringRules.rendered = function(){
 	$('#b1').val(14);
 	$('#a2').val(17);
 	$('#b2').val(35);
+	diffDef.changed();
 }
