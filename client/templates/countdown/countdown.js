ffde4733d4c9d9f426d6eec2d5ae70d9ab219a73
Eric Wyne <ecwyne@gmail.com>
2015-12-14 11:11:43 -0800

    update fromNow

--- client/templates/countdown/countdown.js
+++ client/templates/countdown/countdown.js
@@ -2,7 +2,7 @@ var dateDep = new Tracker.Dependency()
 
 Template.registerHelper('fromNow', function(){
 	dateDep.depend();
-	return moment(new Date('12/20/2015')).fromNow();
+	return moment(new Date('2015-12-19T08:00:00.000Z')).fromNow();
 });
 
 Meteor.setInterval(function(){
