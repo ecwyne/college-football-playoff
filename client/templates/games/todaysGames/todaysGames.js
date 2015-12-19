f3f5b9c8d719f5db8ff6abf5bc4c63d624c024f5
Eric Wyne <ecwyne@gmail.com>
2015-12-19 16:20:42 -0600

    add more info to todays games

---
+++ client/templates/games/todaysGames/todaysGames.js
@@ -0,0 +1,20 @@
+Template.bowlCard.helpers({
+	bowlCardColor: function (){
+		if (this.started){
+			let actual = R.map(R.prop('score'),this.teams);
+			let picks = R.path(['picks', Meteor.userId()], this);
+			let correct = R.gt.apply(R, actual) == R.gt.apply(R, picks);
+			if (!R.equals.apply(R, actual)){
+				return correct ? 'lightgreen' : 'lightred';
+			}
+		}
+	},
+	bowlCardRank: function (){
+		let bowl = this;
+		let myScore = bowl.scoreFor(Meteor.userId());
+		let allScores = Meteor.users.find({'profile.done': true}).map(function (e){
+			return bowl.scoreFor(e._id)
+		}).sort();
+		return allScores.indexOf(myScore) + 1;
+	}
+});
