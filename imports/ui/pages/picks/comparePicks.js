b28aa577df97e0db0269af3f432dd66a97457376
Eric Wyne <ecwyne@gmail.com>
2016-12-03 22:22:51 -0800

    get compare-picks to work with defaults

--- imports/ui/pages/picks/comparePicks.js
+++ imports/ui/pages/picks/comparePicks.js
@@ -6,33 +6,35 @@ import R from 'ramda';
 
 import './comparePicks.html';
 
-Template.comparePicks.rendered = function(){
-	Session.setDefault('compare1', Meteor.user()._id);
+Template.comparePicks.onRendered(() => {
+	Session.setDefault('compare1', Meteor.userId());
 	Session.setDefault('compare2', Meteor.users.findOne({'profile.done': true}, {sort: {'rank.rank': 1}})._id);
-	$('.select2').select2({width: '250px'}).on('change', e => {
-		Session.set(e.currentTarget.id, e.target.value);
-	});
-	$('#compare1').select2('val', get('compare1'));
-	$('#compare2').select2('val', get('compare2'));
-};
+	const c1 = $('#compare1').select2({width: '250px'});
+	const c2 = $('#compare2').select2({width: '250px'});
+	c1.val(get('compare1')).trigger('change');
+	c2.val(get('compare2')).trigger('change');
+	c1.on('change', e => e.target.value && Session.set(e.currentTarget.id, e.target.value));
+	c2.on('change', e => e.target.value && Session.set(e.currentTarget.id, e.target.value));
+});
+
+const get = key => Session.get(key);
+const allNumbers = R.all(R.is(Number));
+const gt = (arr = []) => R.gt(...arr);
 
 Template.comparePicks.helpers({
 	compareClass: function(){
-		return R.gt.apply(R, this.picks[get('compare1')]) == R.gt.apply(R, this.picks[get('compare2')]) ? '' : 'info';
+		return gt(this.picks[get('compare1')]) == gt(this.picks[get('compare2')]) ? '' : 'info';
 	},
 	winnerName: function(id){
 		if (!allNumbers(R.pathOr(['nope'], ['picks', id], this))) {
 			return '';
 		}
-		return R.gt.apply(R, this.picks[id]) ? this.teams[0].name : this.teams[1].name;
+		return gt(this.picks[id]) ? this.teams[0].name : this.teams[1].name;
 	},
 	correctWinnerClass: function(id){
 		if (!this.finished) {
 			return 'label-default';
 		}
-		return R.gt.apply(R, R.map(R.prop('score'), this.teams)) == R.gt.apply(R, this.picks[id]) ? 'label-success' : 'label-danger';
+		return gt(R.map(R.prop('score'), this.teams)) == gt(this.picks[id]) ? 'label-success' : 'label-danger';
 	}
 });
-
-const get = key => Session.get(key);
-const allNumbers = R.all(R.is(Number));
